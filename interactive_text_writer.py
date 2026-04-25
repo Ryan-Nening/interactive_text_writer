3e958a0ef7ccf5921c46699b85e544bd3a46c4d0
Ryan-Nening <neningryan10@gmail.com>
2026-04-26 02:29:13 +0800

    I added the init method to accept the output file name and size the window.

--- interactive_text_writer.py
+++ interactive_text_writer.py
@@ -1,4 +1,10 @@
 import tkinter
 
 class InteractiveTextWriter:
+
+    def __init__(self, output_file_name):
+        self.output_file_name = output_file_name
+        self.main_window = tkinter.Tk()
+        self.main_window.title("Life Writer Tool")
+        self.main_window.geometry("400x200")
     
