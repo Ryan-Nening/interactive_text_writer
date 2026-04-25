5576ad0493a7937198f1b82ae91708afe61277e2
Ryan-Nening <neningryan10@gmail.com>
2026-04-26 02:52:24 +0800

    I placed the save button and the status message label.

--- interactive_text_writer.py
+++ interactive_text_writer.py
@@ -13,4 +13,14 @@ class InteractiveTextWriter:
         self.instruction_label.pack()
         self.user_input_field = tkinter.Entry(self.main_window, width=50)
         self.user_input_field.pack()
+
+    def build_user_interface(self):
+        self.instruction_label = tkinter.Label(self.main_window, text="Enter a line for your life story:")
+        self.instruction_label.pack()
+        self.user_input_field = tkinter.Entry(self.main_window, width=50)
+        self.user_input_field.pack()
+        self.save_button = tkinter.Button(self.main_window, text="Save Line", command=self.save_current_line)
+        self.save_button.pack()
+        self.status_message = tkinter.Label(self.main_window, text="Status: Waiting for input...")
+        self.status_message.pack()
     
