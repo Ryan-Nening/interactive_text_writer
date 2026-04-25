ee4f7a55e09c42f43a3f9a3f2dd37fe9e4816369
Ryan-Nening <neningryan10@gmail.com>
2026-04-26 02:37:57 +0800

    Ibuilt the text entry and instructions for the writer tool.

--- interactive_text_writer.py
+++ interactive_text_writer.py
@@ -7,4 +7,10 @@ class InteractiveTextWriter:
         self.main_window = tkinter.Tk()
         self.main_window.title("Life Writer Tool")
         self.main_window.geometry("400x200")
+
+    def build_user_interface(self):
+        self.instruction_label = tkinter.Label(self.main_window, text="Enter a line for your life story:")
+        self.instruction_label.pack()
+        self.user_input_field = tkinter.Entry(self.main_window, width=50)
+        self.user_input_field.pack()
     
